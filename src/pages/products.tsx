1bfba91bbf01528365f40b658615ce914bd7f539
Umer <25100029@lums.edu.pk>
2023-07-22 23:42:01 +0500

    fb marketplace link added

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Link from 'next/link'
 import { Container, Grid, Flex, Center, Button } from '@mantine/core'
 import Image from 'next/image'
 
@@ -31,16 +31,18 @@ export default function ProductsPage() {
                 <h3>
                     To view more products, here is a link to Our Facebook Marketplace
                 </h3>
-                <Button gradient={{ from: "grape", to: "violet" }} variant="gradient" className='w-48'>
-                    Marketplace
-                </Button>
+                <Link href="https://www.facebook.com/marketplace/profile/100093440680697" target="_blank" rel="noopener noreferrer">
+                    <Button gradient={{ from: "grape", to: "violet" }} variant="gradient" className='w-48'>
+                        Marketplace
+                    </Button>
+                </Link>
             </Center>
 
             <Grid gutter="lg">
                 {products.map((prod, index) => (
                     <Grid.Col key={index} md={6} lg={4} className='flex justify-center'>
                         <div className='p-4 flex flex-col gap-4 transition-all duration-150 ease-in card hover:scale-105 hover:shadow-xl rounded-lg cursor-pointer' style={{ backgroundColor: "#25262b" }}>
-                            <img alt={prod.name} src={`/products/${prod.img}`} width={250}/>
+                            <img alt={prod.name} src={`/products/${prod.img}`} width={250} />
                             <h3>{prod.name}</h3>
                         </div>
                     </Grid.Col>
